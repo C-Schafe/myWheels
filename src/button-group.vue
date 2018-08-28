2af26562cc9971c197c5bac4ace47ecb1d4acf50
louischiang <mingyangchiang@163.com>
2018-08-28 15:00:22 +0800

    完成 button-group

---
+++ src/button-group.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="w-button-group">
+        <slot></slot>
+    </div>
+</template>
+<script>
+
+</script>
+<style lang="scss">
+    .w-button-group {
+        display: inline-flex;
+        vertical-align: middle;
+        > .w-button {
+            border-radius: 0;
+            margin-left: -1px;
+            &:first-child {
+                border-top-left-radius: var(--button-border-radius);
+                border-bottom-left-radius: var(--button-border-radius);
+            }
+            &:last-child {
+                border-top-right-radius: var(--button-border-radius);
+                border-bottom-right-radius: var(--button-border-radius);
+            }
+            &:hover {
+                position: relative;
+                z-index: 1;
+            }
+        }
+    }
+</style>
