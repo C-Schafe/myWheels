5ca085c16dc236aaf28ff12fa157bd394a7d922d
louischiang <mingyangchiang@163.com>
2018-08-28 13:11:25 +0800

    将svg代码整理到组件库

---
+++ src/svg.vue
@@ -0,0 +1,16 @@
+<template>
+    <svg class="w-icon">
+        <use :xlink:href="`#i-${name}`"></use>
+    </svg>
+</template>
+<script>
+    export default {
+        props:['name']
+    }
+</script>
+<style lang="scss">
+    .w-icon {
+        width: 1.3em;
+        height: 1.3em;
+    }
+</style>
