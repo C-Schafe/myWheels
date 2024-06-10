376e9bed59e256715d7b35092ba9a559dc1d1f11
jiangmingyang <louisjiangmy@gmail.com>
2024-06-10 18:02:58 +0800

    refactor: code style change

--- src/sider.vue
+++ src/sider.vue
@@ -1,15 +1,13 @@
 <template>
-    <div class="sider">
-        <slot></slot>
-    </div>
+  <div class="sider">
+    <slot></slot>
+  </div>
 </template>
 <script>
-    export default {
-        name: "wheelsSider"
-    }
+export default {
+  name: "wheelsSider"
+}
 </script>
 <style lang="scss" scoped>
-    .sider {
-
-    }
+.sider {}
 </style>
